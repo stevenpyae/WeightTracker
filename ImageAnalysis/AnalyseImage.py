b0446fde402a3d291a0397407c4cf5c17da21ed6
stevenpyae <steven.pyaelinkyaw@gmail.com>
2024-02-02 17:56:35 +0800

    Extract weight method

--- ImageAnalysis/AnalyseImage.py
+++ ImageAnalysis/AnalyseImage.py
@@ -7,7 +7,7 @@ pytesseract.pytesseract.tesseract_cmd = ('C:/Program Files/Tesseract-OCR/tessera
 img = cv2.imread("Sample Image.jpg")
 # Preprocessing the image starts
 
-blur = cv2.GaussianBlur(img, (5,5), 0)
+blur = cv2.GaussianBlur(img, (5, 5), 0)
 # Convert the image to gray scale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -15,23 +15,39 @@ inverted_gray = cv2.bitwise_not(gray)
 
 '''Finding the perfect filter to get the details out of the photo'''
 # Apply Histogram Equalized technique
-equ = cv2.equalizeHist(inverted_gray)
+equalise_for_date = cv2.equalizeHist(inverted_gray)
+
+equalise_for_weight = cv2.equalizeHist(gray)
 
 # Apply Adaptive Mean thresholding
-extract_date = cv2.adaptiveThreshold(inverted_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 9) #This is to extract the date
+filtered_img_for_date = cv2.adaptiveThreshold(inverted_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15,
+                                              9)  # This is to extract the date
 date_pattern = r"\b\d{2}-\d{2}-\d{4} \d{2}:\d{2}:\d{2} [AP]M\b"
+weight_pattern = r"Weight: (\d+\.\d+)kg"
 
+filtered_img_for_weight = cv2.adaptiveThreshold(inverted_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15,
+                                       8)  # This is to extract weight
 # Histogram Equalized
-cv2.imshow('Histogram Equalized', equ)
+cv2.imshow('Histogram Equalized', equalise_for_date)
 
-cv2.imshow('Adaptive Gaussian', extract_date)
+cv2.imshow('Adaptive Gaussian', filtered_img_for_weight)
 
-text = pytesseract.image_to_string(extract_date)
+weight_text = pytesseract.image_to_string(filtered_img_for_weight)
 
-print(text)
-matches = re.findall(date_pattern, text)
+print(weight_text)
+
+'''
+date_text = pytesseract.image_to_string(extract_date)
+
+matches = re.findall(date_pattern, date_text)
 
 for match in matches:
     print(match)
 
+cv2.waitKey(0)'''
+
+matches = re.findall(weight_pattern, weight_text)
+
+for match in matches:
+    print(match + 'kg')
 cv2.waitKey(0)
