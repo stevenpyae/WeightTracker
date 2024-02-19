f53730b3e1217f49b02d4112c7a14f74cf37fc9d
stevenpyae <steven.pyaelinkyaw@gmail.com>
2024-02-19 21:51:47 +0800

    Modify Get weight function, need to add in additional logic for weight

---
+++ ImageAnalysis/GetWeight.py
@@ -0,0 +1,24 @@
+import re
+from datetime import datetime
+
+
+def get_weight_from_image(image, convert_service):
+    weight_pattern = r"Weight: (\d+\.\d+)kg"
+
+    extracted_text = convert_service.convert_to_text(image)
+
+    matches = re.findall(weight_pattern, extracted_text)
+
+    print(matches)
+    for match in matches:
+        # Check if the match is in the correct format
+        try:
+            # Attempt to parse the match as a weight object
+            parsed_date = datetime.strptime(match, "%d-%m-%Y %I:%M:%S %p")
+            # If parsing succeeds, return the valid date format
+            return match
+        except ValueError:
+            # If parsing fails, continue to the next match
+            pass
+
+    return None
